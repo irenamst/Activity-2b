9eedf1cc664d72db7660085070cfe063d6d8aead
Irena Staneva <irenamst@gmail.com>
2022-02-16 10:31:46 +0200

    Add files via upload

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Activity_2b.Models;
+using Activity_2b.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Activity_2b.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+
+            return View();
+        }
+        public IActionResult ProccessLogin(UserModel userModel)
+        {
+            SecurityServices securityServices = new SecurityServices();
+
+            if(securityServices.IsValid(userModel))
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else
+            {
+                return View("LoginFailure", userModel);
+            }
+        }
+    }
+}
