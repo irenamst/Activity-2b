a723e09e7f094a10c59987d9aa841e9f981fc90c
Irena Staneva <irenamst@gmail.com>
2022-02-16 10:35:57 +0200

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+﻿USE Test
+
+GO
+
+insert into dbo.Users (USERNAME,PASSWORD) values ('BillGates','bigbucks')
+delete from dbo.Users  where USERNAME='BillGates' AND PASSWORD='bigbicks'
+select * from dbo.Users
